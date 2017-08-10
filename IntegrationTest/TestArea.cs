31d927e20ab6f0dc7b201ccdd6a0491723fb0803
mmoroney <mikemoroney1722@hotmail.com>
2017-08-09 20:47:50 -0700

    Added Square test.

--- IntegrationTest/TestArea.cs
+++ IntegrationTest/TestArea.cs
@@ -31,5 +31,30 @@ namespace IntegrationTest
 
             TestUtilities.TestFunctions(1, tolerance, functions);
         }
+
+        [TestMethod]
+        public void TestSquare()
+        {
+            int steps = 1000;
+            double tolerance = 1e-5;
+
+            Func<double, double>[] functions = new Func<double, double>[]
+            {
+                S => S * S,
+                S => new Square(S).IntegrateAreaCartesian((x, y) => 1, steps),
+                S => new Square(S).IntegrateAreaPolar((r, theta) => 1, steps)
+            };
+
+            TestUtilities.TestFunctions(10, tolerance, functions);
+
+            functions = new Func<double, double>[]
+            {
+                S => S * S * S * S / 4,
+                S => new Square(S).IntegrateAreaCartesian((x, y) => x * y, steps),
+                S => new Square(S).IntegrateAreaPolar((r, theta) => r * r * Math.Cos(theta) * Math.Sin(theta), steps)
+            };
+
+            TestUtilities.TestFunctions(1, tolerance, functions);
+        }
     }
 }
---
+++ NumericalIntegration/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalIntegration
+{
+    public class Square : Area
+    {
+        public double Size { get; private set; }
+        public Square(double size)
+        {
+            this.Size = size;
+        }
+
+        public override double IntegrateAreaCartesian(Func<double, double, double> f, int steps)
+        {
+            return Integration.Integrate(f, new Integral1(() => 0, () => this.Size, steps),
+                new Integral2(x => 0, x => this.Size, steps));
+        }
+
+        public override double IntegrateAreaPolar(Func<double, double, double> f, int steps)
+        {
+            return IntegrateAreaCartesian((x, y) => f(Math.Sqrt(x * x + y * y), x == 0 ? Math.PI / 2 : Math.Atan(y / x)), steps);
+        }
+    }
+}
